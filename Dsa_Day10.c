5a66f73d5c4b73543607392bb54294e8b72e85ab
tejasahlawat16-droid <tejasahlawat16@gmail.com>
2026-03-22 23:35:23 +0530

    Create Dsa_Day10.c

---
+++ Dsa_Day10.c
@@ -0,0 +1,47 @@
+/* Problem: Read a string and check if it is a palindrome using two-pointer comparison.
+
+Input:
+- Single line: string s
+
+Output:
+- Print YES if palindrome, otherwise NO
+
+Example:
+Input:
+level
+
+Output:
+YES
+
+Explanation: String reads same forwards and backwards
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    int left, right;
+    int isPalindrome = 1;
+
+    // Read input string
+    scanf("%s", s);
+
+    left = 0;
+    right = strlen(s) - 1;
+
+    while (left < right) {
+        if (s[left] != s[right]) {
+            isPalindrome = 0;
+            break;
+        }
+        left++;
+        right--;
+    }
+
+    if (isPalindrome)
+        printf("YES");
+    else
+        printf("NO");
+
+    return 0;
+}
