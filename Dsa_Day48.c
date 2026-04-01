0e35df6253c9ea00be2069e250390a20ef5961ed
tejasahlawat16-droid <tejasahlawat16@gmail.com>
2026-04-01 18:18:25 +0530

    Create Dsa_Day48.c

---
+++ Dsa_Day48.c
@@ -0,0 +1,22 @@
+/*Problem: Count Leaf Nodes
+
+Implement the solution for this problem.
+
+Input:
+- Input specifications
+
+Output:
+- Output specifications
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+// Count Leaf Nodes implementation
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    // Implementation here
+    printf("Result\n");
+    return 0;
+}
