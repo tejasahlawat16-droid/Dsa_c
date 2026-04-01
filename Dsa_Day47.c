9416bacdfb7ecbbe9daf76a4aa829e043c8e1353
tejasahlawat16-droid <tejasahlawat16@gmail.com>
2026-04-01 18:16:53 +0530

    Create Dsa_Day47.c

---
+++ Dsa_Day47.c
@@ -0,0 +1,22 @@
+/* Problem: Height of Binary Tree
+
+Implement the solution for this problem.
+
+Input:
+- Input specifications
+
+Output:
+- Output specifications
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+// Height of Binary Tree implementation
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    // Implementation here
+    printf("Result\n");
+    return 0;
+}
