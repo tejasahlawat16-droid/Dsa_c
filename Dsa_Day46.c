0d649670002a99087a9eef4178d5a1c031f43457
tejasahlawat16-droid <tejasahlawat16@gmail.com>
2026-04-01 18:14:53 +0530

    Create Dsa_Day46.c

---
+++ Dsa_Day46.c
@@ -0,0 +1,22 @@
+/* Problem: Level Order Traversal
+
+Implement the solution for this problem.
+
+Input:
+- Input specifications
+
+Output:
+- Output specifications
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+// Level Order Traversal implementation
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    // Implementation here
+    printf("Result\n");
+    return 0;
+}
