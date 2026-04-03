2f5258b3f47623a3d09e8cf12149b6f01c1b13dd
tejasahlawat16-droid <tejasahlawat16@gmail.com>
2026-04-03 16:53:50 +0530

    Update Dsa_Day51.c

--- Dsa_Day51.c
+++ Dsa_Day51.c
@@ -18,3 +18,70 @@ Input:
 Output:
 6
 */
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of Tree Node
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+// Create new node
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+// Insert into BST
+struct TreeNode* insert(struct TreeNode* root, int val) {
+    if (root == NULL)
+        return newNode(val);
+
+    if (val < root->val)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+// Find LCA in BST
+int lowestCommonAncestor(struct TreeNode* root, int p, int q) {
+    while (root != NULL) {
+        if (p < root->val && q < root->val)
+            root = root->left;
+        else if (p > root->val && q > root->val)
+            root = root->right;
+        else
+            return root->val;
+    }
+    return -1;
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    struct TreeNode* root = NULL;
+    int value;
+
+    // Read BST nodes
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &value);
+        root = insert(root, value);
+    }
+
+    int p, q;
+    scanf("%d %d", &p, &q);
+
+    // Find and print LCA
+    int lca = lowestCommonAncestor(root, p, q);
+    printf("%d\n", lca);
+
+    return 0;
+}
