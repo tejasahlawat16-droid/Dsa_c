65c9e9e7a42847a434311600fab7ed82a3fd6598
tejasahlawat16-droid <tejasahlawat16@gmail.com>
2026-04-03 16:50:40 +0530

    Create Dsa_Day51.c

---
+++ Dsa_Day51.c
@@ -0,0 +1,20 @@
+/* Problem Statement:
+Find the Lowest Common Ancestor (LCA) of two nodes in a Binary Search Tree.
+
+Input Format:
+- First line contains integer N
+- Second line contains N space-separated integers
+- Third line contains two node values
+
+Output Format:
+- Print the LCA value
+
+Example:
+Input:
+7
+6 2 8 0 4 7 9
+2 8
+
+Output:
+6
+*/
