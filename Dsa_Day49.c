0c063476156d94c46d9cf8902fd29a8246b9ec56
tejasahlawat16-droid <tejasahlawat16@gmail.com>
2026-04-03 16:43:26 +0530

    Create Dsa_Day49.c

---
+++ Dsa_Day49.c
@@ -0,0 +1,22 @@
+/* Problem: BST Insert
+
+Implement the solution for this problem.
+
+Input:
+- Input specifications
+
+Output:
+- Output specifications
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+// BST Insert implementation
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    // Implementation here
+    printf("Result\n");
+    return 0;
+}
