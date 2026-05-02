a5e2ac6ccc2ca57c48ab656d3ea2bf97770782b7
tejasahlawat16-droid <tejasahlawat16@gmail.com>
2026-05-02 23:59:49 +0530

    Create Dsa_Day92.c

---
+++ Dsa_Day92.c
@@ -0,0 +1,55 @@
+/* Problem: Implement Quick Sort using partition method (Lomuto or Hoare).
+*/
+#include <stdio.h>
+
+// Swap function
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Lomuto partition function
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];   // Choose last element as pivot
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
+}
+
+// Quick sort function
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    quickSort(arr, 0, n - 1);
+
+    // Print sorted array
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
